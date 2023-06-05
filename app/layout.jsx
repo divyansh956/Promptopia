8b98759713eebb79e94dc6a375a67397f1b9801a
Divyansh Saxena <divyanshsxn998@gmail.com>
2023-06-05 10:08:36 +0530

    Added User Login

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@app/globals.css';
 import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
   title: 'Promptopia',
@@ -11,15 +12,17 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
 
-        <div className='main'>
-          <div className='gradient' />
-        </div>
+        <Provider>
+          <div className='main'>
+            <div className='gradient' />
+          </div>
 
-        <main className='app'>
-          <Nav />
-          {children}
-        </main>
-
+          <main className='app'>
+            <Nav />
+            {children}
+          </main>
+          
+        </Provider>
       </body>
     </html>
   )
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useEffect, useState } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
-    getProviders();
+    setUpProviders();
   }, [])
 
   return (
@@ -36,7 +36,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link
               href='/create-prompt'
@@ -77,7 +77,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
               src='/assets/images/logo.svg'
