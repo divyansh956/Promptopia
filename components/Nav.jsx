6175edb21803dab815f811ad0775d0276fefa489
Divyansh Saxena <divyanshsxn998@gmail.com>
2023-06-05 10:25:29 +0530

    Final Changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const { data: session } = useSession();
@@ -12,18 +12,15 @@ const Nav = () => {
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setUpProviders = async () => {
-      const response = await getProviders();
-
-      setProviders(response);
-    }
-    setUpProviders();
-  }, [])
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href='/'
-        className='flex gap-2 flex-center'>
+      <Link href='/' className='flex gap-2 flex-center'>
         <Image
           src='/assets/images/logo.svg'
           alt='logo'
@@ -38,24 +35,22 @@ const Nav = () => {
       <div className='sm:flex hidden'>
         {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
-            <Link
-              href='/create-prompt'
-              className='black_btn'>
+            <Link href='/create-prompt' className='black_btn'>
               Create Post
             </Link>
-            <button
-              type='button'
-              onClick={signOut}
-              className='outline_btn'>
-              Sign Out</button>
+
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
 
             <Link href='/profile'>
               <Image
-                src='/assets/images/logo.svg'
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
-                alt='profile'></Image>
+                alt='profile'
+              />
             </Link>
           </div>
         ) : (
@@ -65,10 +60,12 @@ const Nav = () => {
                 <button
                   type='button'
                   key={provider.name}
-                  onClick={() =>
-                    (provider.id)}
-                  className='black_btn'>
-                  Sign In
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className='black_btn'
+                >
+                  Sign in
                 </button>
               ))}
           </>
@@ -80,12 +77,12 @@ const Nav = () => {
         {session?.user ? (
           <div className='flex'>
             <Image
-              src='/assets/images/logo.svg'
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
               alt='profile'
-              onClick={() => setToggleDropdown((prev) => (!prev))}
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
 
             {toggleDropdown && (
@@ -135,7 +132,6 @@ const Nav = () => {
           </>
         )}
       </div>
-
     </nav>
   );
 };
